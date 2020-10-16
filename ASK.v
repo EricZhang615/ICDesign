851079c2added3a44828abaa63a200e33c9413ba
EricZhang615 <eric20000615@gmail.com>
2020-10-16 12:27:58 +0800

    debug

--- ASK.v
+++ ASK.v
@@ -1,21 +1,21 @@
 module ASK (
   input rst,
-  input CarryWave,
-  input BaseSig,
-  output wire Modulated
+  input [9:0] CarryWave,
+  input [2:0] BaseSig,
+  output wire [10:0] Modulated
   );
 
   reg en=0;
 
   always @ ( posedge rst ) begin
     if (rst) begin
-      en = 1;
-    end else begin
       en = 0;
+    end else begin
+      en = 1;
     end
   end
 
-  assign Modulated = (CarryWave && BaseSig) & en;
+  assign Modulated = (CarryWave && BaseSig);
 
 
 endmodule // ASK
